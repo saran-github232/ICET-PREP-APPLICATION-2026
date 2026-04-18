d373e26682be31a59dc26abca6d04fbb1dbd8735
Lakshmi Saran Polisetti <149070064+saran-github232@users.noreply.github.com>
2026-04-18 18:01:23 +0530

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ICET-PREP-APPLICATION-2026/',
+  base: process.env.VERCEL ? '/' : '/ICET-PREP-APPLICATION-2026/',
   plugins: [
     react(),
     tailwindcss(),
